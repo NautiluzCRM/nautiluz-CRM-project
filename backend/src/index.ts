7849a880f34c9b34e3b957e571e65356c7ea4800
BrunoKazuya <bruno.kazuya.yamato@gmail.com>
2025-12-16 14:00:29 -0300

    alterações descartadas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 
 import pipelineRoutes from './routes/Pipeline.routes'; 
-import leadRoutes from './routes/Lead.routes'
+import leadRoutes from './routes/Lead.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/pipeline', pipelineRoutes);
 app.use('/leads', leadRoutes);
+
+const PORT = 3000; 
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
