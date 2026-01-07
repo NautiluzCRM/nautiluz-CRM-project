6a2381e520594ce1255b5b5d68a05e135bf9d3b1
Felipe da Costa Coqueiro <felipe.coqueiro@usp.br>
2026-01-06 21:55:31 -0300

    fix(scroll): add scroll vertical em cada coluna kanban

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -200,7 +200,7 @@ const Index = () => {
         </div>
 
         {/* Kanban Board */}
-        <div className="flex-1 overflow-hidden">
+        <div className="flex-1 min-h-0 overflow-hidden">
           {pipeline ? (
             <KanbanBoard
               colunas={pipeline.colunas}
